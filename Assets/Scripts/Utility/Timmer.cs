af0f5571bd8c16a2847f61310d68648cf55eedf2
Hanjo92 <gooldare@naver.com>
2023-09-13 15:37:44 +0900

    유틸리티 함수 추가, SimpleFramwork 수정
    SimpleFramwork Invoker, LateUpdate 추가

---
+++ Assets/Scripts/Utility/Timmer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Timmer
+{
+	private readonly float endTime;
+	private float fTime;
+	public bool enable { get; private set; }
+
+	public Action onTimeEnd;
+
+	public Timmer(float _endTime, float _fTime = 0, Action timeEndAction = null)
+	{
+		endTime = _endTime;
+		fTime = _fTime;
+		onTimeEnd = timeEndAction;
+		enable = false;
+	}
+
+	public void Init()
+	{
+		fTime = 0;
+		enable = false;
+	}
+
+	public void Resume()
+	{
+		enable = true;
+	}
+
+	public void Start(float _fTime = 0)
+	{
+		fTime = _fTime;
+		enable = true;
+	}
+
+	public void Stop()
+	{
+		enable = false;
+	}
+	public void Update(float _fTime)
+	{
+		if(enable)
+			fTime += _fTime;
+		if(fTime > endTime)
+		{
+			onTimeEnd?.Invoke();
+		}
+	}
+}
