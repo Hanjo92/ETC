d68a36542d37ae57a9a0bbb5d54ed17dce4dc227
Hanjo92 <gooldare@naver.com>
2023-09-13 15:58:14 +0900

    Update SmokeGrenade.cs

--- Assets/Smoke/Scripts/SmokeGrenade.cs
+++ Assets/Smoke/Scripts/SmokeGrenade.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
-
+using SimpleFramework;
 public class SmokeGrenade : UpdateObject
 {
 	public enum EruptionShape
